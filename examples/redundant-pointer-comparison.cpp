757bd9efa57404b76cbe31866c9e9b837ee3dfc1
Gabriel Matute <gmatute@umich.edu>
2019-12-13 14:37:48 -0500

    Fix redundant ptr comp, add more tests.

--- examples/redundant-pointer-comparison.cpp
+++ examples/redundant-pointer-comparison.cpp
@@ -1,5 +1,24 @@
-void func(const int* ptr) {
+int *ptr;
+
+void not_null() {
+	if (ptr != nullptr) {
+		// Do something
+	}
+}
+
+void is_null() {
 	if (ptr == nullptr) {
 		// Do something
 	}
+}
+
+void compound_expr(bool other) {
+	if (ptr == nullptr || other) {
+		// Do something
+	}
+}
+
+bool nomatch() {
+	compound_expr(ptr != nullptr);
+	return ptr == nullptr;
 }
